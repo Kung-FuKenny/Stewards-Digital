6d30c4cbbc281d99903b58b49cb9a9830c639308
Kenny M. Ngoie <mukonkolekngoie@yahoo.com>
2024-07-13 21:25:35 +0200

    Re-add static files

--- vite.config.ts
+++ vite.config.ts
@@ -15,3 +15,20 @@ export default defineConfig({
   },
   base: '/Hospital-Website',
 });
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist',
+//     rollupOptions: {
+//       input: {
+//         main: './index.html'
+//       }
+//     }
+//   },
+//   server: {
+//     port: 3000
+//   }
+// });
