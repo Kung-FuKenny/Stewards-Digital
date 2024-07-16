d95420376152784eaa9724f3ab4afcc9ea58f47c
Kenny M. Ngoie <mukonkolekngoie@yahoo.com>
2024-07-16 09:49:40 +0200

    vite.config.ts & vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {}
-  },
-  base: '/'
+  base: '/',
 });
+
 
 // import { defineConfig } from 'vite';
 // import react from '@vitejs/plugin-react';
